e0b0372498139f5b08b153dc0fce3da3535572f3
doaa-oi <ddoaa2489@gmail.com>
2025-07-15 14:20:11 +0200

    Integrate backend with frontend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Branch;
+use App\Models\Client;
+use App\Models\Family;
+use App\Models\Passport;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +18,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(10)->create();
+        Branch::factory(10)->create();
+        User::factory(10)->create();
+        Client::factory(10)->create();
+        Family::factory(10)->create();
+        Passport::factory(10)->create();
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
