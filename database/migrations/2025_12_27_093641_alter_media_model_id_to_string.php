6ce4962402cdd9bd84ddd230039ba7aa79ccf688
Alaa-almsalaty <alaa.almsalaty@gmail.com>
2025-12-27 11:44:11 +0200

    Update 2025_12_27_093641_alter_media_model_id_to_string.php

--- database/migrations/2025_12_27_093641_alter_media_model_id_to_string.php
+++ database/migrations/2025_12_27_093641_alter_media_model_id_to_string.php
@@ -8,19 +8,19 @@ return new class extends Migration
     public function up(): void
     {
         // Drop index created by morphs()
-        DB::statement('ALTER TABLE media DROP INDEX media_model_id_model_type_index');
+        DB::statement('ALTER TABLE media DROP INDEX media_model_type_model_id_index');
 
         // Change model_id type to string (UUID safe)
         DB::statement('ALTER TABLE media MODIFY model_id VARCHAR(255) NOT NULL');
 
         // Recreate index
-        DB::statement('CREATE INDEX media_model_id_model_type_index ON media (model_id, model_type)');
+        DB::statement('CREATE INDEX media_model_type_model_id_index ON media (model_id, model_type)');
     }
 
     public function down(): void
     {
-        DB::statement('ALTER TABLE media DROP INDEX media_model_id_model_type_index');
+        DB::statement('ALTER TABLE media DROP INDEX media_model_type_model_id_index');
         DB::statement('ALTER TABLE media MODIFY model_id BIGINT UNSIGNED NOT NULL');
-        DB::statement('CREATE INDEX media_model_id_model_type_index ON media (model_id, model_type)');
+        DB::statement('CREATE INDEX media_model_type_model_id_index ON media (model_id, model_type)');
     }
 };
